5e30b6caa7608c75e5ac70c442d51ef71651a6ee
muhuanlegend <muhuanlegend@gmail.com>
2025-04-16 19:05:56 +0000

    featurecard implemented

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 import NavBar from "./components/NavBar";
 import LogoSection from "./components/LogoSection";
+import FeatureCard from "./sections/FeatureCard";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <ShowcaseSection />
       <LogoSection />
+      <FeatureCard />
     </>
   );
 };
