d3e57250c46a22024bb5512b3edf379687c44525
muhuanlegend <muhuanlegend@gmail.com>
2025-04-17 05:43:21 +0000

    experience done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ShowcaseSection from "./sections/ShowcaseSection";
 import NavBar from "./components/NavBar";
 import LogoSection from "./components/LogoSection";
 import FeatureCard from "./sections/FeatureCard";
+import ExperienceSection from "./sections/ExperienceSection";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <ShowcaseSection />
       <LogoSection />
       <FeatureCard />
+      <ExperienceSection />
     </>
   );
 };
